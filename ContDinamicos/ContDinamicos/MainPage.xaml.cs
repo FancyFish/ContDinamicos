6815c466f20edf48a40b249af4cfc68060e28e14
FancyFish <boanergtwin1995@hotmail.com>
2018-05-30 19:55:09 -0500

    commit

---
+++ ContDinamicos/ContDinamicos/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContDinamicos
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            BtnGenerar.Clicked += BtnGenerar_Clicked;
+		}
+
+        private void BtnGenerar_Clicked(object sender, EventArgs e)
+        {
+            Panel.Children.Add(new Button()
+            {
+                Text = $"boton{Panel.Children.Count + 1}"
+            });
+        }
+    }
+}
